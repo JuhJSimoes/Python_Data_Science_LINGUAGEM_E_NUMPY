efd190eb9660723be0e89a9c63f1383a30185d26
JuhJSimoes <julianajsimoes@gmail.com>
2022-10-16 16:33:34 -0300

    Introducao_Numpy_Listas_Condicionais_Laços

---
+++ Aula_Numpy_Condicionais_Lacos.py
@@ -0,0 +1,28 @@
+Acessorios = ['Rodas de liga', 'Travas elétricas', 'Piloto automático', 'Bancos de couro', 'Ar condicionado', 'Sensor de estacionamento', 'Sensor crepuscular', 'Sensor de chuva']
+print(Acessorios)
+
+for item in Acessorios:
+    print(item)
+    
+print('\n')
+    
+list(range(10))
+
+for i in range(10):
+    print(i ** 2)
+
+print('\n')
+    
+quadrado = []
+for i in range(10):
+    quadrado.append(i ** 2)
+print(quadrado)
+
+print('\n')
+
+#List Comprehensions
+print([i ** 2 for i in range(10)])
+
+print('\n')
+
+#Loops aninhados
